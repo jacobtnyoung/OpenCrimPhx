e4c9041254e4aac23eff80f588a9e82c1d34bbf8
jacobtnyoung <CRJ605@asu.edu>
2025-05-22 16:04:01 -0700

    Update phx_arrest_traffic_race.R

--- phx_arrest_traffic_race.R
+++ phx_arrest_traffic_race.R
@@ -37,7 +37,6 @@ dat <- read.csv(
 # ----
 
 dat_race <- dat  |>
-  select( SIMPLE_SUBJ_RE_GRP, UNIQUE_NAME_ID, ISSUING_OFFICER )  |>  # take the variables you need
   group_by( UNIQUE_NAME_ID )  |>  # group by unique id
   filter( n() > 1 ) |>  # drop cases that have only 1 incident
   filter( UNIQUE_NAME_ID != "MNI-" ) |> # drop cases that have an incorrect id
@@ -55,6 +54,60 @@ df_inconsistent <- dat_race %>%
   filter( UNIQUE_NAME_ID %in% race_variation$UNIQUE_NAME_ID ) 
 
 100 * ( dim( race_variation )[1] / dim( race_gt1_incident )[1] )
+
+
+
+
+
+
+race_switchers <- dat %>%
+  group_by( UNIQUE_NAME_ID ) %>%
+  filter( UNIQUE_NAME_ID != "MNI-" ) |> # drop cases that have an incorrect id
+  filter( n_distinct( SUBJ_RACE ) > 1 ) %>%
+  ungroup()
+
+
+race_switchers %>%
+  count( UNIQUE_NAME_ID ) %>%
+  summarise( mean_n = mean( n ), max_n = max( n ) )
+
+race_switchers %>%
+  count( UNIQUE_NAME_ID, SUBJ_RACE ) %>%
+  group_by( UNIQUE_NAME_ID ) %>%
+  summarise( races = toString( unique( SUBJ_RACE ) ) ) %>%
+  head()
+
+
+
+
+# IV approach
+
+ambiguous_individuals <- dat %>%
+  group_by( UNIQUE_NAME_ID ) %>%
+  filter( n_distinct( SIMPLE_SUBJ_RE_GRP ) > 1 ) %>%
+  ungroup()
+
+dat_iv <- ambiguous_individuals %>%
+  left_join( officer_race_tendency, by = "ISSUING_OFFICER" ) %>%
+  mutate( race_is_black = as.integer( SIMPLE_SUBJ_RE_GRP == "Black" ) )
+
+first_stage <- feols(
+  race_is_black ~ pct_black + HOUR_OF_DAY,
+  data = dat_iv
+)
+
+summary( feols(
+  CRIMINAL_TRAFFIC_VIOLATIONS ~ 1 | race_is_black ~ pct_black + HOUR_OF_DAY,
+  data = dat_iv
+) )
+
+
+
+Think about including controls here
+Work through the logic, does this make sense?
+  Think about what you are trying to get at
+
+
 
 
 
